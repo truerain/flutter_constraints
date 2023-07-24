e2a058b018755b0036583f67751ac8484c2eb56c
Y.W_Shin <yunwoo.shin@gmail.com>
2023-07-24 08:33:11 +0000

    코드카피완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -131,6 +132,59 @@ class _TestConstraintsState extends State<TestConstraints> {
             Expanded(child: listViewOfFlutterLogo),
           ],
         );
+      case 7:
+        return const UnconstrainedBox(child: FlutterLogo(size: 600));
+      case 8:
+        return const OverflowBox(
+          minWidth: 0,
+          minHeight: 0,
+          maxWidth: double.infinity,
+          maxHeight: double.infinity,
+          child: FlutterLogo(size: 600),
+        );
+      case 9:
+        return UnconstrainedBox(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 200, maxHeight: 200),
+            child: const FlutterLogo(size: 600),
+          ),
+        );
+      case 10:
+        return const UnconstrainedBox(
+          child: LimitedBox(
+            maxWidth: 100,
+            maxHeight: 100,
+            child: FlutterLogo(size: 600),
+          ),
+        );
+      case 11:
+        return const Center(
+          child: LimitedBox(
+            maxWidth: 100,
+            maxHeight: 100,
+            child: FlutterLogo(size: 600),
+          ),
+        );
+      case 12:
+        return Column(children: listToTest);
+      case 13:
+        return ListView(children: listToTest);
+      case 14:
+        return Center(
+          child: Transform.rotate(
+            angle: math.pi / 4,
+            child: Opacity(
+              opacity: 0.5,
+              child: Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                color: Colors.yellow,
+                child: const Text("한글연습", style: TextStyle(fontSize: 64)),
+              ),
+            ),
+          ),
+        );
     }
 
     return Container();
@@ -143,4 +197,37 @@ class _TestConstraintsState extends State<TestConstraints> {
           FlutterLogo(size: 100),
         ],
       );
+
+  List<Widget> get listToTest => [
+        const FlutterLogo(size: 25),
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          width: 50,
+          color: Colors.green,
+          child: const FlutterLogo(size: 25),
+        ),
+        Container(
+          height: 50,
+          color: Colors.blue,
+          child: const FlutterLogo(size: 25),
+        ),
+        Container(
+          alignment: Alignment.bottomRight,
+          height: 50,
+          color: Colors.yellow,
+          child: const FlutterLogo(size: 25),
+        ),
+        const Align(
+          alignment: Alignment.bottomRight,
+          child: FlutterLogo(size: 25),
+        ),
+        LayoutBuilder(builder: (context, constraints) {
+          return Text('$constraints',
+              style: const TextStyle(color: Colors.white));
+          //BoxConstraints(0.0<=w<=300, 0<=h<=Infinity) in Column
+          //BoxConstraints(w==300, 0<=h<=Infinity) in ListView
+        }),
+      ];
 }
